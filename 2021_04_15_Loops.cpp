6352806a73cc6ffaa2b220bf7d5d0650e542601f
ITRabbitx <64525602+ITRabbitx@users.noreply.github.com>
2021-04-16 09:26:55 +0200

    Add files via upload
    While and For loop drill

---
+++ 2021_04_15_Loops.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	
+int x = 0; 	
+	
+while (x < 10) {
+	cout << x << endl;
+	x++;
+	}
+
+
+
+
+for ( int i = 10; i > 0; i--) {
+	cout << i << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0; 	
+
+}
