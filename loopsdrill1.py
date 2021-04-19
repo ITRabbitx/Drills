9ef7f031dae478f063469e5d9fd47458ab93aa41
ITRabbitx <64525602+ITRabbitx@users.noreply.github.com>
2021-04-19 20:57:30 +0200

    Add files via upload
    Loops in python excercise

---
+++ loopsdrill1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 20:50:36 2021
+
+@author: Eva S
+"""
+
+for x in range(0, 11): 
+	print (x);
+	x += 1; 
+	
+	
+y = 0;
+	
+while y < 11:
+	print(y);
+	y += 1;	
