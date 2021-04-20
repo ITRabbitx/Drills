10275f264243a8fdd077a2dfb6446722e23bf4ee
ITRabbitx <64525602+ITRabbitx@users.noreply.github.com>
2021-04-20 21:25:08 +0200

    Add files via upload
    loops in C++

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    for ( int i = 0; i<11; i++ ){
+        cout << i << endl;
+    }
+
+    int x = 10;
+    while (x<21) {
+        cout << x << endl;
+        x++;
+    }
+
+
+
+    return 0;
+}
