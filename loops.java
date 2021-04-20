6e909dd2e9e0799e0ee1fd19c01cf8c60d8ab788
ITRabbitx <64525602+ITRabbitx@users.noreply.github.com>
2021-04-20 20:56:59 +0200

    Add files via upload
    Loops drill in java

---
+++ loops.java
@@ -0,0 +1,18 @@
+package ch.loops.drill;
+
+public class loops {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+		System.out.println(i);
+		}
+		
+		int x = 10;
+		while (x < 21) {
+			System.out.println(x);
+			x++;
+		}
+	
+	}
+}
