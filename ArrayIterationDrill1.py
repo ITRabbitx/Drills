3797fc5684ea3d2ab8969f54c6d5354177226f10
ITRabbitx <64525602+ITRabbitx@users.noreply.github.com>
2021-04-23 19:03:23 +0200

    Add files via upload
    Iterating over an array

---
+++ ArrayIterationDrill1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 18:47:27 2021
+
+@author: Eva S
+"""
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+x = 0
+
+while x < 2:
+	x = x + 1
+	for i in array: 
+		print(i)
+	
+	
+
+	
+	
